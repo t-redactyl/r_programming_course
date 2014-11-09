22b2ef313f045cb20ff23e431ef678f6744a66f8
bdelloidea <jodie.burchell@gmail.com>
2014-11-09 22:52:07 +1100

    Completed swirl assignment 1

--- Swirl programming assignment.R
+++ Swirl programming assignment.R
@@ -1,3 +1,13 @@
 # Program: swirl programming assignment
 # Project: R programming Coursera
 # Author: Jodie, November 2014
+
+# installing/loading the package:
+if(!require(installr)) { 
+  install.packages("installr"); require(installr)} #load / install+load installr
+
+# using the package:
+updateR() # this will start the updating process of your R installation.  It will check for newer versions, and if one is available, will guide you through the decisions you'd need to make.
+
+install.packages("swirl")
+Jodie
